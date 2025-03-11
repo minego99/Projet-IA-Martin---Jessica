300d69ab9c761a44f54c94491f63d36103192fe4
minego99 <61887025+minego99@users.noreply.github.com>
2025-03-11 13:15:22 +0100

    first content for cubee model

--- cubee/gamemodel.py
+++ cubee/gamemodel.py
@@ -1,21 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 10 12:17:49 2025
-
-@author: marti
-"""
+import random
 
 
 class CubeeGameModel():
-    def __init__(self):
-        print()
-        
-
-class CubeeHuman():
-    def __init__(self):
-        print()
-        
-
+    def __init__(self,dimension, player1, player2):
+        self.dimension = dimension
+        self.gameboard = [[0] * self.dimension for i in range(self.dimension)]
+        self.gameboard[0][0] = 1
+        self.gameboard[self.dimension-1][self.dimension-1] = 2
+        print(self.gameboard)
+        self.players = [player1, player2]
+        self.current_player = 0
+    def shuffleplayers(self):
+        random.shuffle(self.players)        
 class CubeePlayer():
+    def __init__(self, player_name):
+        self.player_name = player_name
+    def play(self):
+        return random.choice([1,2,3,4])        
+        
+
+class CubeeHuman(CubeePlayer):
     def __init__(self):
         print()
+        
+
+newModel = CubeeGameModel(5, 0, 0)
+
