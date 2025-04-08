06c8f73fb18a66ec6d819d474a4dfae7cf56f561
minego99 <61887025+minego99@users.noreply.github.com>
2025-04-08 10:24:32 +0200

    add karts const

--- pixel_kart/const.py
+++ pixel_kart/const.py
@@ -5,4 +5,10 @@ PIXEL_TYPES = {
         "GRASS":{"color":"green", "letter":"G"},
         "WALL":{"color":"black", "letter":"W"},
         "FINISH":{"color":"yellow", "letter":"F"}
+        
+    }
+PLAYER_TYPES = {
+    "PLAYER1":{"color":"red","letter":"1"},
+    "PLAYER2":{"color":"blue","letter":"2"},
+    "PLAYER3":{"color":"pink","letter":"3"}
     }
